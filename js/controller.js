89b97734b313af4dbba834a31bdddeecca546996
Mac Eisenberg <maceisen@gmail.com>
2014-09-14 18:50:24 -0700

    add new students

--- js/controller.js
+++ js/controller.js
@@ -2,4 +2,8 @@ var studentRoster = angular.module('studentRoster', []);
 
 studentRoster.controller('StudentCtrl', function StudentCtrl($scope) {
   $scope.students = [];
+  $scope.addStudent = function() {
+    $scope.students.push({"name": $scope.studentName});
+    $scope.studentName = null;
+  };
 });

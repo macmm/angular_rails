6510ed799f40b8ecb91e5aed31bcd53767cc1129
Mac Eisenberg <maceisen@gmail.com>
2014-09-14 20:26:48 -0700

    add root route

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
 var studentRoster = angular.module('studentRoster', ['ngRoute']);
+
+studentRoster.config(function ($routeProvider) {
+   $routeProvider
+      .when('/',
+      {
+        controller: 'StudentCtrl',
+        templateUrl: 'views/Students.html'
+      })
+ });

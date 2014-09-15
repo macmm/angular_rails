d04ef6838a3985f6f45fe457990855760562212a
Mac Eisenberg <maceisen@gmail.com>
2014-09-14 18:49:21 -0700

    remove sample data

--- js/controller.js
+++ js/controller.js
@@ -1,11 +1,5 @@
 var studentRoster = angular.module('studentRoster', []);
 
 studentRoster.controller('StudentCtrl', function StudentCtrl($scope) {
-  $scope.students = [
-    { "name":"Sam Schmidt" },
-    { "name":"Jessica Martin" },
-    { "name":"Sandy Smith" },
-    { "name":"Ryan Samuels" },
-    { "name":"Brentwood Davis" }
-  ]
+  $scope.students = [];
 });
